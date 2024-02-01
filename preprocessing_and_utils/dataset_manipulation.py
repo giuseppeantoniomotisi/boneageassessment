9b3658c3538358ca1401c0d0892d8d60610d5b3a
Giuseppe <Giuseppe@172-100-012-031.res.spectrum.com>
2024-02-01 21:29:55 +0100

    dataset_gender_splitter function

--- preprocessing_and_utils/dataset_manipulation.py
+++ preprocessing_and_utils/dataset_manipulation.py
@@ -1,29 +1,62 @@
+"""Organizes image datasets based on gender information provided in a CSV file. """
+
 import os
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import cv2 as cv
-#import tensorflow.keras.backend as K
 
-def dataset_gender_splitter(training_path: str, csv_file: str, split = True):
-    if split == True:
-        # Create directories for male and female images if they don't exist
-        male_dir = os.path.join(training_path, 'male')
-        female_dir = os.path.join(training_path, 'female')
+def dataset_gender_splitter(send_dir: str, csv_file: str, split: bool = True):
+    """
+    This function reads a CSV file containing gender information associated with image files.
+    It organizes the image files into separate directories based on gender.
+    If `split` is True, it splits the dataset into separate directories for male and female images.
+    If `split` is False, it merges the dataset, moving images from male and female directories back
+    to the source directory.
 
-        if not os.path.exists(male_dir):
-            os.makedirs(male_dir)
-        if not os.path.exists(female_dir):
-            os.makedirs(female_dir)
+    Args:
+    - send_dir (str): The directory path where the images and CSV file are located.
+    - csv_file (str): The name of the CSV file containing information about the images.
+    - split (bool, optional): If True, split the dataset based on gender; if False, merge it.
+    Default is True.
 
-        # Read the CSV file into a DataFrame
-        ds = pd.read_csv(os.path.join(training_path, csv_file))
+    Returns:
+    None
+    """
+    # Read the CSV file into a DataFrame
+    dataset = pd.read_csv(os.path.join(send_dir, csv_file))
 
-        # Iterate through each row of the DataFrame
-        for _, row in ds.iterrows():
-        # Extract filename and determine gender
+    # Create directories for male and female images if they don't exist
+    male_dir = os.path.join(send_dir, 'male')
+    female_dir = os.path.join(send_dir, 'female')
+
+    if not os.path.exists(male_dir):
+        os.makedirs(male_dir)
+    if not os.path.exists(female_dir):
+        os.makedirs(female_dir)
+
+    if split:
+        # Split dataset based on gender
+        for _, row in dataset.iterrows():
+            # Extract filename and determine gender
             filename = f"{row['id']}.png"
-         # Determine the destination directory based on gender
+            # Determine the destination directory based on gender
             dest_dir = male_dir if row['male'] else female_dir
-        # Move the image file to the appropriate directory
-            os.replace(os.path.join(training_path, filename), os.path.join(dest_dir, filename))
+            # Move the image file to the appropriate directory
+            os.replace(os.path.join(send_dir, filename), os.path.join(dest_dir, filename))
+    else:
+        # Merge dataset
+        for _, row in dataset.iterrows():
+            # Extract filename and determine gender
+            filename = f"{row['id']}.png"
+            # Determine the destination directory based on gender
+            dest_dir = male_dir if row['male'] else female_dir
+            # Move the image file to the appropriate directory
+            os.replace(os.path.join(dest_dir, filename), os.path.join(send_dir, filename))
+
+        # Remove empty male and female directories
+        os.remove(male_dir)
+        os.remove(female_dir)
+
+
+if __name__ == '__main__':
+    SENDER = '../boneage-training-dataset' #boneage-training-dataset
+    FILENAME = 'train.csv' #csv file
+    dataset_gender_splitter(send_dir=SENDER, csv_file=FILENAME, split=True)
