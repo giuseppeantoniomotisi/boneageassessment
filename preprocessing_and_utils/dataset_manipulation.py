e00459c193124aaa4deb033f36eaf784ad15c089
Giuseppe Antonio Motisi <113900742+giuseppeantoniomotisi@users.noreply.github.com>
2024-02-02 09:18:55 +0100

    Update dataset_manipulation.py

--- preprocessing_and_utils/dataset_manipulation.py
+++ preprocessing_and_utils/dataset_manipulation.py
@@ -11,15 +11,15 @@ csv_file: The name of the CSV file containing information about the images.
 split: A boolean parameter indicating whether to split the dataset based on gender.
 
 It reads the CSV file into a DataFrame using pandas.read_csv().
-It creates two directories for male and female images inside the send_dir directory if 
-they don't already exist. If split is True, it iterates through the rows of the DataFrame 
-and moves the image files to the appropriate directory based on the gender information 
+It creates two directories for male and female images inside the send_dir directory if
+they don't already exist. If split is True, it iterates through the rows of the DataFrame
+and moves the image files to the appropriate directory based on the gender information
 provided in the CSV file. If split is False, it reverses the process by moving the images 
 back to the source directory from their respective male or female directories.
 If merging the dataset, it removes the empty male and female directories.
 
-In the if __name__ == '__main__': block, the code specifies the sender directory 
-(SENDER) and the CSV filename (FILENAME) and then calls the dataset_gender_splitter function 
+In the if __name__ == '__main__': block, the code specifies the sender directory
+(SENDER) and the CSV filename (FILENAME) and then calls the dataset_gender_splitter function
 with the specified parameters.
 
 Usage:
@@ -35,7 +35,7 @@ import pandas as pd
 def dataset_gender_splitter(send_dir: str, csv_file: str, split: bool = True):
     """
     Organizes image datasets based on gender information provided in a CSV file.
-    
+
     This function reads a CSV file containing gender information associated with image files.
     It organizes the image files into separate directories based on gender.
     If `split` is True, it splits the dataset into separate directories for male and female images.
