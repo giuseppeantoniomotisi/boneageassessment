22fd5685d1e625173dd0002c80023e2e2c7fc568
giuseppeantoniomotisi <motisigiuseppeantoni@yahooo.it>
2024-03-21 12:28:21 +0100

    add licence at top

--- baa/boneageassessment.py
+++ baa/boneageassessment.py
@@ -1,3 +1,22 @@
+# Copyright (C) 2024 motisigiuseppeantonio@yahoo.it
+#
+# For the license terms see the file LICENSE, distributed along with this
+# software.
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; either version 2 of the License, or (at your
+# option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
 """
 Bone Age Assessment System
 
@@ -128,7 +147,6 @@ def prediction_module(opt:bool,name:str,path:str):
     Returns:
         prediction_result (type): The result of prediction.
     """
-    utils.houdini(opt='weights')
     if opt:
         if not os.path.exists(os.path.join(path, name)):
             raise FileNotFoundError("image was not found! Please check it.")
--- baa/prediction.py
+++ baa/prediction.py
@@ -1,3 +1,22 @@
+# Copyright (C) 2024 motisigiuseppeantonio@yahoo.it
+#
+# For the license terms see the file LICENSE, distributed along with this
+# software.
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; either version 2 of the License, or (at your
+# option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
 """Predict the age of an individual based on an input image.
 """
 import argparse
@@ -25,7 +44,7 @@ def process(image_name:str, image_path:str, save:bool=True) -> str:
 
     # First preprocess the image
     prep_instance = Preprocessing()
-    preprocessed_image = prep_instance.preprocessing_image(image_name=image, save=save)
+    preprocessed_image = prep_instance.preprocessing_image(image_name=image, save=save, show=False)
 
     # Now load model and make prediction
     baa_instance = BoneAgeAssessment()
